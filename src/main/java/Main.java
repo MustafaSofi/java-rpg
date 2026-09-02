99cc3c8471894c582d2b71ccffda919142846dd2
Mustafa Sofi <birdsoapy@gmail.com>
2026-09-02 12:50:16 +0300

    Added Healing and Game End + fixed alot of bugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
-//day = 1
-//hours = 1
+//day = 2
+//hours = 4.5
 
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 
+		int playerBlock = 0;
 		int strength = 3;
 		int armor = 3;
 		int playerHealth = 100;
@@ -15,23 +15,24 @@ public class Main {
 		int playerDamage = strength * 5;
 		int choice = 0;
 		int healingPotion = 3;
-		int playerBlock = armor * 3;
-		int goblingDamage = strength * 4;
-		int damageTaken = 0;
+		int goblinDamage = strength * 4;
+		int damageTaken =0 ;
+
 		System.out.println("A Goblin just came to you, what to do?");		
 
 
-		while (playerHealth > 0) {
+		while (playerHealth > 0 && goblinHealth > 0) {
 
-			System.out.print("1. Attack\n2. Block\n3.Heal \n Enter a Number: ");
+			System.out.print("1. Attack\n2. Block\n3. Heal \n Enter a Number: ");
 
 			choice = scanner.nextInt();			
 
-
+			playerBlock = 0;
+			damageTaken = Math.max(0, goblinDamage - playerBlock);
 			if (choice == 1) {
 
 				System.out.println("You attacked the goblin");
-				goblinHealth -= playerDamage  ;
+				goblinHealth =Math.max(0, goblinHealth - playerDamage) ;
 				System.out.println("You Damaged the Gobling for: "+playerDamage+" HP\n"+"Goblin health: "+goblinHealth);
 
 
@@ -39,15 +40,47 @@ public class Main {
 			else if (choice == 2) {
 
 				System.out.println("You chose to Block");
-				damageTaken = goblingDamage - playerBlock;
-				playerHealth  -= damageTaken;
-				System.out.println("You Blocked "+playerBlock+"hitpoints");
-				System.out.println("Taken Damage: "+damageTaken+" HP\n"+"Your Health is: "+playerHealth);
-
+				playerBlock = armor * 3; 
+				System.out.println("You Blocked "+playerBlock+" hitpoints");
+				damageTaken =Math.max(0, goblinDamage - playerBlock);
 			}
 
+			else if (choice == 3) {
+				System.out.println("You chose to heal");
+				if (playerHealth>= 100){
+					System.out.println("Your health is already full");
+					continue;
+				}
+				else if (healingPotion <= 0){ 
+					System.out.println("you have no healing potions");
+					continue;
+				}
+				else{
+					healingPotion--;
+					playerHealth =Math.min(100, playerHealth+15);
 
-		System.out.println("Goblin is still standing!!!!");
+					System.out.println("You Heal for 15 HP -1 Healing Potion\nYou have "+healingPotion+" Healing Potions left.\nYour Health is: "+playerHealth+" HP ");
+				}
+			}	
+
+
+			if (goblinHealth > 0) {
+
+
+				System.out.println("Goblin turn: He hit you with "+damageTaken+" HP");
+				playerHealth = Math.max(0 , playerHealth - damageTaken);
+				System.out.println("Your health is: "+playerHealth);
+			}
+
+			if (goblinHealth == 0)
+				System.out.println("You win!!, Goblin is Dead :)");
+			else
+				System.out.println("Be Careful, Goblin is still standing!!");
+
+			if (playerHealth == 0) 
+
+				System.out.println("You Died, Try Again.");	
+
 
 		}
 

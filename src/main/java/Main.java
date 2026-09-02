98e51f501e0f6010518ec6696206f22acf04a532
Mustafa Sofi <birdsoapy@gmail.com>
2026-09-02 13:53:38 +0300

    Added Random Damage, Critical Hit and fixed a typo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,26 @@
 //day = 2
 //hours = 4.5
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+
 
 		int playerBlock = 0;
 		int strength = 3;
 		int armor = 3;
 		int playerHealth = 100;
 		int goblinHealth = 70;
-		int playerDamage = strength * 5;
+		int playerDamage = 0; 
 		int choice = 0;
 		int healingPotion = 3;
-		int goblinDamage = strength * 4;
+		int goblinDamage = 0;
 		int damageTaken =0 ;
+		int crit = 0;
 
 		System.out.println("A Goblin just came to you, what to do?");		
 
@@ -27,13 +31,33 @@ public class Main {
 
 			choice = scanner.nextInt();			
 
+			// 1 in a 10 chance to get crit
+			crit = random.nextInt(1, 11);
+			
+			//Calculate Random damage (12 - 21 )
+			goblinDamage = strength * random.nextInt(4, 8);
+			playerDamage = strength * random.nextInt(4, 8);
+			// reset Block
 			playerBlock = 0;
+
+			if (crit == 3)
+				playerDamage *=2;
+
+			if (crit == 2)
+				goblinDamage *=2;
+
+
 			damageTaken = Math.max(0, goblinDamage - playerBlock);
+
+
 			if (choice == 1) {
+
+				if (crit == 3)
+					System.out.println("Critical HIT!! x2 Damage");
 
 				System.out.println("You attacked the goblin");
 				goblinHealth =Math.max(0, goblinHealth - playerDamage) ;
-				System.out.println("You Damaged the Gobling for: "+playerDamage+" HP\n"+"Goblin health: "+goblinHealth);
+				System.out.println("You Damaged the Goblin for: "+playerDamage+" HP\n"+"Goblin health: "+goblinHealth);
 
 
 			}
@@ -61,11 +85,18 @@ public class Main {
 
 					System.out.println("You Heal for 15 HP -1 Healing Potion\nYou have "+healingPotion+" Healing Potions left.\nYour Health is: "+playerHealth+" HP ");
 				}
+			}else{
+
+				System.out.println("Please enter a number from 1 to 3");
+				continue;
+
 			}	
 
 
 			if (goblinHealth > 0) {
 
+				if (crit == 2)
+					System.out.println("Critical HIT!! x2 Goblin Damage");
 
 				System.out.println("Goblin turn: He hit you with "+damageTaken+" HP");
 				playerHealth = Math.max(0 , playerHealth - damageTaken);
